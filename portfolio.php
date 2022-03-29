fe6329594332dae67937453edd343b9ec90dedd6
rumzum <99711964+rumzum@users.noreply.github.com>
2022-03-29 16:52:31 +0600

    Add files via upload

---
+++ portfolio.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible"content="IE=edge".
+  <title>My Portfolio</title>
+  <style>
+  
+     img{
+		 max-width:100%
+	    border:5px dashed blue;
+		height:500px;
+	    display:block;
+	    margin-left:auto;
+	    margin-right:auto;
+	    width:40%
+	 
+	 }
+	 </style>
+  <meta name="description"content="">
+  <meta name="viewport"content="width=device-width,initial-scale=1">
+  <link rel="stylesheet"href="">
+ </head>
+ <body style="background-colour:pink;">
+ 
+ <div class="head">
+   <a href="active"href="#home">Home</a>
+   <a href="#About">About</a>
+   <a href="#Skills">Skills</a>
+ <hi>Hello,I am Rumzum</h1>
+ <img src="aiub.jpg">
+ 
+ 
+ 
+ </body>
+ </html>
+ 
+   
+ 
+ 
